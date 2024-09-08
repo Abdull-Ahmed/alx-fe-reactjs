d9c92fdd25cc2a4944de112778e1a7cb92e07a55
Abdullahi sharif <abdullahi0.sharif0@gmail.com>
2024-09-08 19:11:50 +0300

    Tailwind installed and configured

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  theme: { extend: {} },
+  variants: { extend: {} },
+  plugins: [],
+};
